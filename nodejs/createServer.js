c9ffeb9142aa3a49e4936cf199e9d11d5a6ce205
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-03-02 16:12:05 +0530

    create server

---
+++ nodejs/createServer.js
@@ -0,0 +1,16 @@
+var http=require('http');
+var host='127.0.0.1';
+var port=3000;
+
+var server=http.createServer((response,request)=>{
+    response.writeHead(200,{"Content-Type":"text/plain"});
+    console.log("server is working");
+    response.end("sever working success");
+})
+
+server.listen(port,host,(error)=>{
+    if(error){
+        return console.log('Error Occurred '+error);
+    }
+    console.log('server is listening on '+host+' : '+port);
+})
