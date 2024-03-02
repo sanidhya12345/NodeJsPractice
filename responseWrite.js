5f273201eed27005a3c876d295c6a116c612ce0c
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-03-02 16:31:20 +0530

    update

---
+++ responseWrite.js
@@ -0,0 +1,16 @@
+var http=require('http');
+var port=3000;
+var host='127.0.0.1';
+
+var server=http.createServer((response,request)=>{
+    response.writeHead(200,{"Content-Type":"text/plain"});
+    response.write("Hello I am learning Node js");
+    response.end("server is fine working");
+});
+
+server.listen(port,host,(error)=>{
+    if(error){
+        return console.log("Error"+error);
+    }
+    console.log("server is listening on "+host+":"+port);
+});
