2aee773cdc0b98d20f276805048d41fd2170e1ab
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-03-10 20:28:13 +0530

    mongo js

--- mongo.js
+++ mongo.js
@@ -1,10 +1,34 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb');
 
-// Create Instance of MongoClient for mongodb
-const client = new MongoClient('mongodb://localhost:27017')
+// Connection URI
+const uri = 'mongodb://localhost:27017';
 
-// Connect to database
-client.connect()
-    .then(() => console.log('Connected Successfully'))
-    .catch(error => console.log('Failed to connect', error))
+// Database Name
+const dbName = 'my_database';
 
+// Create a new MongoClient
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function main() {
+    try {
+        // Connect the client to the server
+        await client.connect();
+        console.log('Connected to MongoDB');
+
+        // Specify the database to be used
+        const db = client.db(dbName);
+
+        // Perform operations with the connected client
+        // For example, you can insert documents into a collection
+        const collection = db.collection('my_collection');
+        await collection.insertOne({ key: 'value' });
+        console.log('Document inserted');
+
+    } finally {
+        // Close the client
+        await client.close();
+    }
+}
+
+// Call the main function
+main().catch(console.error);
